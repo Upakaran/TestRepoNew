f0215329cde480ead392da6c869c6886f464c86c
Upakaran <upakaran.0407@gmail.com>
2020-08-27 21:05:59 +0530

    initial

---
+++ src/package1/SampleClass1.java
@@ -0,0 +1,31 @@
+package package1;
+
+public class SampleClass1 {
+
+	public static void main(String[] args) {
+		
+		int hellCount = 0, yeahCount = 0;
+		
+		for(int i = 0; i <=100 ; i=i+5){
+			System.out.print(i+" ");
+			if(i%20==0){
+				System.out.println("Hell");
+				hellCount++;
+			}
+				
+			else{
+				System.out.println("Yeah");
+				yeahCount++;
+			}
+				
+		}
+		System.out.println();
+		System.out.println("Hell "+hellCount+" Yeah "+yeahCount);
+		
+		
+		// line added in Branch 1
+
+		// new line from remote
+	}
+
+}
